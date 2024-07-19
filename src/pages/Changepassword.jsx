dcfc7843cc7c16ae02a878998dcd3e6371dcb8a6
Raisya Syaifania <ThofikhBisyron>
2024-07-19 23:27:04 +0700

    feat: add responsive in Changepassword

--- src/pages/Changepassword.jsx
+++ src/pages/Changepassword.jsx
@@ -20,37 +20,37 @@ function Changepassword(){
 
 
     return(
-        <div class="bg-[#F4F7FF]">
+        <div className="md:bg-[#F4F7FF] bg-white">
             <Navbar/>
-            <div class="flex mt-[50px]">
-                <div class="w-[30%]">
-                    <div class="flex flex-col w-[242px] h-[508px] ml-[70px]">
-                        <div class="flex mb-[50px]">
-                        <img src={profile} alt="" class="w-[55px] h-[55px]" />
+            <div className="flex mt-[50px]">
+                <div className="md:w-[30%] md:block hidden ">
+                    <div className="flex flex-col w-[242px] h-[508px] ml-[70px]">
+                        <div className="flex mb-[50px]">
+                        <img src={profile} alt="" className="w-[55px] h-[55px]" />
                         <div>
                             <div>Jhon Thomson</div>
                             <div>Entrepreneur, ID</div>
                         </div>
                         </div>
-                        <div class="flex flex-col gap-[30px]">
-                            <div class="flex gap-[25px]"><img src={profilejhon} alt="" />Profile</div>
-                            <div class="flex gap-[25px] ml-[49px]"><img src={cardprofile} alt="" />Card</div>
-                            <div class="flex gap-[25px] ml-[49px]"><img src={blueedit} alt="" />Edit Profile</div>
-                            <div class="flex gap-[25px] ml-[49px]"><img src={changep} alt="" />Change Password</div>
-                            <div class="flex gap-[25px]"><img src={book} alt="" />My Booking</div>
-                            <div class="flex gap-[25px]"><img src={wishlist} alt="" />My Wishlist</div>
-                            <div class="flex gap-[25px]"><img src={setting} alt="" />Settings</div>
-                            <div class="flex gap-[25px]"><img src={logout} alt="" />Logout</div>
+                        <div className="flex flex-col gap-[30px]">
+                            <div className="flex gap-[25px]"><img src={profilejhon} alt="" />Profile</div>
+                            <div className="flex gap-[25px] ml-[49px]"><img src={cardprofile} alt="" />Card</div>
+                            <div className="flex gap-[25px] ml-[49px]"><img src={blueedit} alt="" />Edit Profile</div>
+                            <div className="flex gap-[25px] ml-[49px]"><img src={changep} alt="" />Change Password</div>
+                            <div className="flex gap-[25px]"><img src={book} alt="" />My Booking</div>
+                            <div className="flex gap-[25px]"><img src={wishlist} alt="" />My Wishlist</div>
+                            <div className="flex gap-[25px]"><img src={setting} alt="" />Settings</div>
+                            <div className="flex gap-[25px]"><img src={logout} alt="" />Logout</div>
                         </div>
                     </div>
                 </div>
-                <div class="flex w-[70%] bg-[#FFFFFF] rounded-[30px] mr-[70px]">
-                    <div class="w-[876px] h-[413px] mt-[46px] ml-[46px]">
-                        <div class ="text-lg mb-[50px]">Change Password</div>
-                        <div class="flex justify-between">Old Password <input type="password" name="oldpassword" class="h-[55px] w-[100%] border rounded-[15px] mb-[33px]" /></div>
-                        <div class="flex justify-between">New Password <input type="password" name="oldpassword" class="h-[55px] w-[100%] border rounded-[15px] mb-[33px]" /></div>
-                        <div class="flex justify-between">Confirm Password<input type="password" name="oldpassword" class="h-[55px] w-[100%] border rounded-[15px] mb-[33px]" /></div>
-                        <button class="w-[100%] h-[61px] bg-blue-600 rounded-[15px] text-white">Update</button>
+                <div className="flex w-[100%] bg-[#FFFFFF] rounded-[30px] mr-[70px] md:w-[70%]">
+                    <div className="w-[876px] h-[413px] mt-[46px] ml-[46px]">
+                        <div className ="text-lg mb-[50px]">Change Password</div>
+                        <div className="flex flex-col md:flex-row justify-between"><div>Old Password</div> <input type="password" name="oldpassword" placeholder="Input Old Password.." className="h-[55px] w-[90%] md:w-[100%] border rounded-[15px] mb-[33px] mr-4 pl-4" /></div>
+                        <div className="flex flex-col md:flex-row justify-between"><div>New Password</div> <input type="password" name="newpassword" placeholder="Input New Password.." className="h-[55px] w-[90%] md:w-[100%] border rounded-[15px] mb-[33px] mr-4 pl-4" /></div>
+                        <div className="flex flex-col md:flex-row justify-between"><div>Confirm Password</div><input type="password" name="confirmpassword" placeholder="Input New Password.." className="h-[55px] w-[90%] md:w-[100%] border rounded-[15px] mb-[33px] mr-4 pl-4" /></div>
+                        <button className="md:w-[100%] h-[61px] bg-blue-600 rounded-[15px] text-white w-[90%]">Update</button>
                     </div>
                 </div>
 
